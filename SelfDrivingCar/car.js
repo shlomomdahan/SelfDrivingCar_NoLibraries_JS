dd7009e55bbe52cc2df664943b98fc74b361243a
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2022-10-28 10:48:43 -0400

    Initial commit

--- SelfDrivingCar/car.js
+++ SelfDrivingCar/car.js
@@ -15,6 +15,10 @@ class Car {
     }
 
     update() {
+        this.#move();
+    }
+
+    #move() {
         if (this.controls.forward){
             this.speed += this.acceleration;
         }
@@ -52,7 +56,6 @@ class Car {
 
         this.x -= Math.sin(this.angle)*this.speed;
         this.y -= Math.cos(this.angle)*this.speed;
-
     }
 
     draw(ctx) {
